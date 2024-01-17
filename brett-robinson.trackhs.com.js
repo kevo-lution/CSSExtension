20d3a18b6eb316e754604ddd6a882ae45e4acd3e
Kevin Richardson <70816004+kevo-lution@users.noreply.github.com>
2024-01-16 22:30:32 -0600

    Add files via upload

---
+++ brett-robinson.trackhs.com.js
@@ -0,0 +1,15 @@
+/* •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••• */
+/* Right here, you have access to the `injector` global object. */
+/* So you can inject unlimited codes using: ••••••••••••••••••• */
+/* injector.injectInlineJs(); ••••••••••••••••••••••••••••••••• */
+/* injector.injectExternalJs(); ••••••••••••••••••••••••••••••• */
+/* injector.injectInternalCss(); •••••••••••••••••••••••••••••• */
+/* injector.injectExternalCss(); •••••••••••••••••••••••••••••• */
+/* Anyway it's OPTIONAL, you can also define your logic. •••••• */
+/* •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••• */
+injector.injectInternalCss(`
+    html {
+        background: black;
+        filter: invert(1) hue-rotate(180deg);
+    }
+`);
